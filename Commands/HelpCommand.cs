940ab3535adfdb2439313ee52f4cf24dc9b1322a
skyjoshua <mcjoshuagaming@outlook.com>
2026-02-28 22:01:12 +0000

    added source command to help command

--- Commands/HelpCommand.cs
+++ Commands/HelpCommand.cs
@@ -7,7 +7,9 @@ public static class HelpComamnd
     public static async Task Execute(Dictionary<long, Channel> channelCache, long channelId, String prefix, string memberPing)
     {
         string helpMessage = $@"**Reactor Commands**:
-        - `{prefix}help` - Shows this list.";
+        - `{prefix}help` - Shows this list.
+        - `{prefix}source` - Shows my source code!
+        ";
 
         if (channelCache.TryGetValue(channelId, out var channel))
         {
